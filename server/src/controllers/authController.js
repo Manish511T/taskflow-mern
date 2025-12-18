156621ecc4bf068ef9a492ce4430a61a544944e8
Manish <mkthakur2301@gmail.com>
2025-12-19 01:10:45 +0530

    feature: add user login api with jwt authentication

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res)=>{
     try{
@@ -33,5 +34,43 @@ export const registerUser = async (req, res)=>{
         });
     }catch(error){
         res.status(500).json({message:"Server error"});
+    }
+};
+
+export const loginUser = async (req, res)=>{
+    try{
+        const {email, password} = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({message:"Email and password are required"});
+        }
+
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+
+        const token = jwt.sign(
+            {userId: user._id},
+            process.env.JWT_SECRET,
+            {expiresIn: "7d"}
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email,
+            },
+        });
+    }catch(error){
+        res.status(500).json({message:"Server Error!"});
     }
 };
