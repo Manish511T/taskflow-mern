c074c48ecc079d57ae3efeb39345d0cffcbb4720
Manish <mkthakur2301@gmail.com>
2025-12-19 01:30:01 +0530

    feature: add jwt auth middleware and protected route

---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+const authmiddleware = (req, res, next)=>{
+    const authHeader = req.headers.authorization;
+
+
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+        return res.status(401).json({message:"Not authorized"});
+    }
+
+    try{
+        const token = authHeader.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.userId;
+        next();
+    }catch(error){
+        return res.status(401).json({message:"Invalid token"});
+    }
+};
+
+export default authmiddleware;
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
+import authmiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/me", authmiddleware, (req,res)=>{
+    res.json({
+        message: "Protected route accessed",
+        userId: req.userId,
+    });
+});
 
 export default router;
