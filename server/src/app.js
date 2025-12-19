5219df3563586c386f7c18436d1aa901ec90ccb7
Manish <mkthakur2301@gmail.com>
2025-12-20 02:24:52 +0530

    feature: register project routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
+import projectRoutes from "./routes/projectRoutes.js"
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/projects", projectRoutes);
 
 app.get("/", (req, res)=>{
     res.send("TaskFlow API is running");
