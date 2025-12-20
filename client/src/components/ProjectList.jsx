ac8fde212618ae28c67861e67ecf5d0199627af3
Manish <mkthakur2301@gmail.com>
2025-12-20 13:50:10 +0530

    feature: add project list component

---
+++ client/src/components/ProjectList.jsx
@@ -0,0 +1,17 @@
+const ProjectList = ({ projects }) => {
+    if (projects.length === 0) {
+        return <p>No projects yet</p>;
+    }
+
+    return (
+        <ul>
+            {projects.map((project) => (
+                <li key={project._id}>
+                    <strong>{project.name}</strong> – {project.description}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default ProjectList
