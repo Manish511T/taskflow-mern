dfb8be3b7da10958aac8909bcfebb0c71b8cbb68
Manish <mkthakur2301@gmail.com>
2025-12-20 15:54:27 +0530

    feature: add project detail, update and delete routes

--- server/src/routes/projectRoutes.js
+++ server/src/routes/projectRoutes.js
@@ -1,10 +1,20 @@
 import express from "express";
 import authmiddleware from "../middlewares/authMiddleware.js";
-import { createProject, getMyProjects } from "../controllers/projectController.js";
+import { 
+    createProject,
+    getMyProjects,
+    getProjectById,
+    updateProject,
+    deleteProject,
+ } from "../controllers/projectController.js";
 
 const router = express.Router();
 
 router.post("/", authmiddleware,createProject);
 router.get("/", authmiddleware, getMyProjects);
+
+router.get("/:id", authmiddleware, getProjectById);
+router.put("/:id", authmiddleware, updateProject);
+router.delete("/:id", authmiddleware, deleteProject);
 
 export default router;
