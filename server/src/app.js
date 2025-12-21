71d0d6b989d31005eb262b37f77eee64eb0ae127
Manish <mkthakur2301@gmail.com>
2025-12-22 00:27:33 +0530

    feature: add list routes under projects

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js"
+import listRoutes from "./routes/listRoutes.js";
 const app = express();
 
 app.use(cors());
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
+app.use("/api/projects", listRoutes);
 
 app.get("/", (req, res)=>{
     res.send("TaskFlow API is running");
---
+++ server/src/routes/listRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import authmiddleware from "../middlewares/authMiddleware.js";
+import {
+    createList,
+    getListByProject,
+} from "../controllers/listController.js";
+
+const router = express.Router();
+
+router.post("/:projectId/lists", authmiddleware, createList);
+router.getE("/:projectId/lists", authmiddleware, getListByProject);
+
+export default router;
