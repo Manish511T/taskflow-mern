00e9911e8c7d575bf7bdf55239137f0ab359eda6
Manish <mkthakur2301@gmail.com>
2025-12-24 00:03:03 +0530

    feature: add update and delete task services

--- client/src/services/taskService.js
+++ client/src/services/taskService.js
@@ -28,3 +28,29 @@ export const createTask = async(listId,task)=>{
 
     return res.json();
 };
+
+export const updateTask = async(taskId, updates)=>{
+    const res = await fetch(`http://localhost:5000/api/tasks/${taskId}`,{
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getToken()}`
+        },
+        body: JSON.stringify(updates)
+    });
+
+    if(!res.ok) throw new Error("Failed to update task");
+    return res.json();
+};
+
+
+export const deleteTask = async(taskId)=>{
+    const res = await fetch(`http://localhost:5000/api/tasks/${taskId}`,{
+        method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${getToken()}`
+        },
+    });
+
+    if(!res.ok) throw new Error("Failed to delete task")
+}
